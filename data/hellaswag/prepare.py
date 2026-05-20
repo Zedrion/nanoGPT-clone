e4918b4d2838448d2568b0f2f591680bc3bc605b
Zedrion <bacccupaccc@gmail.com>
2026-05-21 11:42:16 +1200

    train bin val bin

--- data/hellaswag/prepare.py
+++ data/hellaswag/prepare.py
@@ -5,13 +5,17 @@ import numpy as np
 from datasets import load_dataset
 
 # load the hellaswag dataset, train split
+print("Loading Rowan/hellaswag train split")
 data = load_dataset("Rowan/hellaswag", split="train")
 
+# Only take the first 1/10 of the dataset
+print("Selecting all rows")
 n = len(data)
-train_dataset = data.select(range(int(n*0.9 / 20)))
-val_dataset = data.select(range(int(n*0.9 / 20), int(n*0.9 / 20) + int(n*0.1 / 20)))
+train_dataset = data.select(range(int(n*0.9 / 10)))
+val_dataset = data.select(range(int(n*0.9 / 10), int(n*0.9 / 10) + int(n*0.1 / 10)))
 
 # convert to text: context + correct ending
+print("Converting to text")
 train_data = ""
 for row in train_dataset:
     endings = row["endings"]
@@ -39,6 +43,7 @@ for row in val_dataset:
     )
 
 # encode with tiktoken gpt2 bpe
+print("Encoding with tiktoken gpt2 bpe")
 enc = tiktoken.get_encoding("gpt2")
 train_ids = enc.encode_ordinary(train_data)
 val_ids = enc.encode_ordinary(val_data)
@@ -46,10 +51,11 @@ print(f"train has {len(train_ids):,} tokens")
 print(f"val has {len(val_ids):,} tokens")
 
 # export to bin files
+print("Exporting to bin files")
 train_ids = np.array(train_ids, dtype=np.uint16)
 val_ids = np.array(val_ids, dtype=np.uint16)
 train_ids.tofile(os.path.join(os.path.dirname(__file__), 'train.bin'))
 val_ids.tofile(os.path.join(os.path.dirname(__file__), 'val.bin'))
 
-# train has 6,844,947 tokens
-# val has 997,592 tokens
+# train has 383,545 tokens
+# val has 42,698 tokens
